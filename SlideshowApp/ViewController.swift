b2cb4adad0165f9b071bf96d9991ce90fb4079d9
seiya.takita <seiya_takita@r.recruit.co.jp>
2023-03-09 16:59:13 +0900

    再生/停止ボタンの動きを実装

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var previousButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    
+    private let playButtonText = "再生"
+    private let pauseButtonText = "停止"
+    
+    private var timer: Timer!
     
     private var imageList: [UIImage] = []
     
     private var selectedIndex = 0
+    
+    private var isPlayingSlideshow = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +37,17 @@ class ViewController: UIViewController {
         imageList.append(UIImage(named: "ramenImage1.jpg")!)
         imageList.append(UIImage(named: "ramenImage2.jpg")!)
         imageList.append(UIImage(named: "ramenImage3.jpg")!)
+    }
+    
+    @objc func updateSlide(_ timer: Timer) {
+        if selectedIndex == imageList.endIndex - 1  {
+            selectedIndex = imageList.startIndex
+            imageView.image = imageList[selectedIndex]
+            return
+        }
+        
+        selectedIndex += 1
+        imageView.image = imageList[selectedIndex]
     }
     
     @IBAction func onTouchNextButton(_ sender: Any) {
@@ -51,5 +72,26 @@ class ViewController: UIViewController {
         imageView.image = imageList[selectedIndex]
     }
     
+    @IBAction func onTouchPlayButton(_ sender: Any) {
+        if isPlayingSlideshow {
+            timer.invalidate()
+            timer = nil
+            
+            isPlayingSlideshow = false
+            playButton.setTitle(playButtonText, for: .normal)
+            
+            nextButton.isEnabled = true
+            previousButton.isEnabled = true
+            return
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateSlide(_:)), userInfo: nil, repeats: true)
+        
+        isPlayingSlideshow = true
+        playButton.setTitle(pauseButtonText, for: .normal)
+        
+        nextButton.isEnabled = false
+        previousButton.isEnabled = false
+    }
 }
 
