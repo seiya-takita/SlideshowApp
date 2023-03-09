94bc8d5d6a710a099dbe4970dc0cc2a674bf962c
seiya.takita <seiya_takita@r.recruit.co.jp>
2023-03-09 15:57:18 +0900

    写真の表示を実装

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private var imageList: [UIImage] = []
+    
+    private var selectedIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadImage()
+        
+        imageView.image = imageList[selectedIndex]
     }
-
+    
+    private func loadImage() {
+        imageList.append(UIImage(named: "ramenImage1.jpg")!)
+        imageList.append(UIImage(named: "ramenImage2.jpg")!)
+        imageList.append(UIImage(named: "ramenImage3.jpg")!)
+    }
 
 }
 
